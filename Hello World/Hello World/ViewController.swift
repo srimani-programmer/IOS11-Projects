c503ac1ae9bd1fd6a1819a1d9251494c007614ba
srimani-programmer <srimani.scholar@gmail.com>
2019-04-27 21:03:05 +0530

    The complete iOS Application which runs on Iphone8. AutoLayout Not added yet.

---
+++ Hello World/Hello World/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by sri manikanta on 27/04/19.
+//  Copyright © 2019 sri manikanta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by sri manikanta on 27/04/19.
+//  Copyright © 2019 Sri Manikanta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
